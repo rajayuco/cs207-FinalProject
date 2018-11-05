d1f27299ed70ce91f84356d42f2234ea81f447ca
WANG Yudi <wyd1582@gmail.com>
2018-11-05 13:18:26 -0500

    updated exp test

--- tests/autodiff_math_test.py
+++ tests/autodiff_math_test.py
@@ -37,10 +37,6 @@ def test_exp_result_single():
     x = ad.autodiff('x', 10)
     assert admath.exp(x).val == np.exp(10)
 
-## test division and exponential
-def test_exp_result():
-    assert admath.exp(x).val == np.exp(10)
-
 ## Test logarithm function
 def test_log_result_single():
     x = ad.autodiff('x', 5, 2)
