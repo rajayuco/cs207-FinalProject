70e624db3380c4a4425fffb27eb5e0b30fdf248f
wyd1582 <wyd1582@gmail.com>
2018-12-07 18:14:34 -0500

    revertRevert "added changes"
    This reverts commit 7e8e58e25bb620b44e6e92986ac61a5b66e13a76.

--- autodiffpy/autodiff_math.py
+++ autodiffpy/autodiff_math.py
@@ -240,12 +240,11 @@ def arcsin(ad):
     [0.10016742] {'x': array([1.00503782])}
     """
     try:
-        if list(map(lambda x:x**2>1, ad.val)) == True:
+        if ad.val**2 > 1:
             raise ValueError('Error: invalid value encountered while calculating derivatives.')
         anew = autodiff.autodiff(name=ad.name, val = np.arcsin(ad.val), der = ad.der)
         for key in ad.der:
             anew.der[key] = 1/np.sqrt(1 - ad.val**2)*ad.der[key]
-        anew.back_partial_der = 1/np.sqrt(1 - ad.val**2)
         return anew
     except AttributeError:
         raise AttributeError("Error: input should be autodiff instance only.")
@@ -272,12 +271,11 @@ def arccos(ad):
     [1.36943841] {'x': array([-1.02062073])}
     """
     try:
-        if list(map(lambda x:x**2>1, ad.val)) == True: #ad.val**2 > 1 or 
+        if ad.val**2 > 1:
             raise ValueError('Error: invalid value encountered while calculating derivatives.')
         anew = autodiff.autodiff(name=ad.name, val = np.arccos(ad.val), der = ad.der)
         for key in ad.der:
             anew.der[key] = -1/np.sqrt(1 - ad.val**2)*ad.der[key]
-        anew.back_partial_der =  -1/np.sqrt(1 - ad.val**2)
         return anew
     except AttributeError:
         raise AttributeError("Error: input should be autodiff instance only.")
@@ -308,7 +306,6 @@ def arctan(ad):
         anew = autodiff.autodiff(name=ad.name, val = np.arctan(ad.val), der = ad.der)
         for key in ad.der:
             anew.der[key] = 1/(1+ad.val**2)*ad.der[key]
-        anew.back_partial_der = 1/(1+ad.val**2)
         return anew
     except AttributeError:
         raise AttributeError("Error: input should be autodiff instance only.")
